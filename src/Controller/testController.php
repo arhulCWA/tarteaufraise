4b085feefcff9603e09d2a0951f96c31393e65cd
axelr <axel.rhul@gmail.com>
2023-10-23 15:59:00 +0200

    test

--- src/Controller/testController.php
+++ src/Controller/testController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class testController extends AbstractController
 {
-    public function index()
+    public function index(): \Symfony\Component\HttpFoundation\Response
     {
         return $this->render("test.html.twig");
     }
